a151f564942c14b3b2534ecccedd4f813d35707b
chiphikari <bumblebee.chain55@gmail.com>
2021-11-22 14:27:47 +0000

    11/22-5

--- db/migrate/20211106045421_create_favorites.rb
+++ db/migrate/20211106045421_create_favorites.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :post_summary, null: false, foreign_key: true
+      t.references :user, type: :bigint, null: false, foreign_key: true
+      t.references :post_summary, type: :bigint, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20211106102646_create_post_houses.rb
+++ db/migrate/20211106102646_create_post_houses.rb
@@ -1,7 +1,7 @@
 class CreatePostHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :post_houses do |t|
-      t.references :post_summary, null: false, foreign_key: true
+      t.references :post_summary, type: :bigint, null: false, foreign_key: true
       t.text :link, null: false
 
       t.timestamps
