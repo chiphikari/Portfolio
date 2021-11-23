b998d56837b52f3218d1671ab637da7392b64b70
chiphikari <bumblebee.chain55@gmail.com>
2021-11-23 03:07:40 +0000

    11/23

--- db/migrate/20211106045426_create_post_tags.rb
+++ db/migrate/20211106045426_create_post_tags.rb
@@ -1,8 +1,8 @@
 class CreatePostTags < ActiveRecord::Migration[5.2]
   def change
     create_table :post_tags do |t|
-      t.references :post_summary, type: :bigint, foreign_key: true
-      t.references :tag, type: :bigint, foreign_key: true
+      t.references :post_summary, null: false, foreign_key: true
+      t.references :tag, null: false, foreign_key: true
 
       t.timestamps
     end
